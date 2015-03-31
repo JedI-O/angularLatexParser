f32ed9d19fe186f9768be7a8062fe531ced0b077
Maximilian Stauss <max.stauss@gmail.com>
2015-04-01 01:04:01 +0200

    added options

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
+'use strict';
+
 var htmlparser = require('htmlparser2');
 var domToLatex = require('./lib/domToLatex');
 
-globalLatex = '';
+module.exports = function(parserCall, parserOptions, options) {
 
-var handler = new htmlparser.DomHandler(function (error, dom) {
-  if (!error) {
-    globalLatex = '';
-    for (var i = 0; i < dom.length; i++) {
-      globalLatex += domToLatex(dom[i]);
+  var globalLatex = '';
+
+  var handler = new htmlparser.DomHandler(function (error, dom) {
+    if (!error) {
+      globalLatex = '';
+      console.log('type: ' + options.latexType);
+      for (var i = 0; i < dom.length; i++) {
+        globalLatex += domToLatex(dom[i], options.latexType);
+      }
+      return globalLatex;
     }
-    return globalLatex;
+  });
+
+  var htmlToLatex = new htmlparser.Parser(handler);
+ 
+  switch (parserCall) {
+    case 'parseComplete': 
+      if(parserOptions.length === 1) {
+        htmlToLatex.parseComplete(parserOptions[0]);
+      } else {
+        return null;
+      }
+    break;
   }
-});
+  return globalLatex;
 
-var htmlToLatex = new htmlparser.Parser(handler);
-
-module.exports = htmlToLatex;
+}
